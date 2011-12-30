7a39d0a5f30cbfc94c0547162715e7dc50b276cd
Rahmouni oussama <ousrah@hotmail.com>
2011-12-30 09:16:37 +0100

    premiere sauvegarde

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site1::Application.config.secret_token = 'bf19475e35d08f3cee244a4025a699840e5f137d02cd7aa23653eb9e224048f4769d2421a244f6913d33baa554b4f327ef08d18e0f6f1722c881b827e67c756f'
